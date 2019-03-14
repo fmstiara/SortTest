57aadfb9ae47261ea5130627f60e5660f49e6828
TairaOuchi <taira.o19@gmail.com>
2019-03-14 16:29:21 +0900

    fin

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,2 +1,19 @@
 class RootController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def sort
+    sortValue = params["sortValue"]
+
+    if sortValue == "high"
+      users = User.order(age: "DESC")
+    elsif sortValue == "low"
+      users = User.order(age: "ASC")
+    else
+      users = User.all
+    end
+
+    render json: users
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'root#index'
+  post '/user/sort', to: 'root#sort', as: 'user_sort'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
